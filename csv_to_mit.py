5844efe48efbf5f01768e7b5fc4551ad635b9f95
mfranz715 <78189229+mfranz715@users.noreply.github.com>
2021-03-16 01:57:10 -0400

    Update csv_to_mit.py

--- csv_to_mit.py
+++ csv_to_mit.py
@@ -1,17 +1,28 @@
+# Convert csv file to wfdb
+# MARS Reverse-Engineering
+# University of Maryland Global Campus
+# Authors: Maura Franz
+
+##Before running script, make sure you have downloaded wfdb
+##pip install wfdb
+##if it still does not run, make sure it is the current version (3.3.0)
 
 import pandas as pd
+import re
+import datetime
 import wfdb
 from wfdb.io.record import (csv2mit)
 
-
-def hdrparse(hdr_name):    
+#parse out info from hdr file
+def hdrparse(file_name):    
     ids=[]
     labels=[]
     units=[]
     frequency=[]
-    hdr=open(hdr_name)
+    hdr=open(file_name)
     for line in hdr:
         parts = line.split(",")
+        #added "ID" before the id number to appease csv2mit
         ids.append("ID"+parts[0].strip("{id: "))
         labels.append(parts[1].strip("label: "))
         units.append(parts[2].strip("unit: "))
@@ -20,33 +31,57 @@ def hdrparse(hdr_name):
     return(ids,units,frequency)
 
 #name new csv
-def namecsv(csv_name):
-    position = csv_name.find('.')
-    strip_pos=len(csv_name)-position
+def namecsv(file_name):
+    position = file_name.find('.')
+    strip_pos=len(file_name)-position
     if (position != -1):
-        new_csv_name = csv_name[:-strip_pos] +"_new" + ".csv"
+        new_csv_name = file_name[:-strip_pos] +"_new" + ".csv"
     else:
-        new_csv_name = csv_name + ".csv"
+        new_csv_name = file_name + ".csv"
     return new_csv_name
+
+#find date and time
+def findtimedate(file_name):
+    csv=open(file_name)
+    #find lines that start with four numbers
+    for inline in csv:
+        if (re.match ("^([0-9]{4})", inline)):
+            timeline = inline
+        else:
+            break
+    #split it into a list using comma as delineator (because data is in the first line too)
+    firstline = timeline.split(',')
+    #first portion of the first line is the date and time
+    starttimestring = firstline[0]
+    #split it into list
+    starttimeparts = re.search("(.+) [\S]*$",starttimestring)
+    #single out the first part of the list (date and time)
+    starttime = starttimeparts.group(1)
+    #split time and date into list using space as delineator
+    time_parts=starttime.split(' ')
+    time=time_parts[1]
+    date=datetime.datetime.strptime(time_parts[0],"%Y-%m-%d").date().strftime("%d/%m/%Y")
+    csv.close()
+    return(time, date)
 
 #turn dataframe columns to list, replace empty cells with 0.1
 def repaircolumn(column):
     newcolumn=[]
     for value in column:
         if value == ' ':
-            newcolumn.append(0.1)
-        elif value==float:
+            newcolumn.append(0)
+        elif type(value)==float:
             newcolumn.append(value)
         else:
-            newcolumn.append(value.strip())
+            newcolumn.append(float(value.strip()))
     return newcolumn
 
 #determine sample frequency:
 def samplefrequency(frequency_list):
     frequency_list = [int(i) for i in frequency_list]
-    sample_frequency = 0
+    sample_frequency = 2000000
     for fs in frequency:
-        if fs > sample_frequency:
+        if fs < sample_frequency:
             sample_frequency = fs
     hz= 1000//sample_frequency
     return hz
@@ -61,10 +96,24 @@ hdr_name = input("Enter hdr name: ")
 hdrcolumns=hdrparse(hdr_name)
 #unpack tuple
 (ids, units, frequency)=hdrcolumns
+print("IDS: ", ids)
+print("Units: ", units)
+print("frequency: ", frequency)
 #find new csv name
 new_csv_name = namecsv(csv_name)
+#extract date and time from csv
+timedate=findtimedate(csv_name)
+#unpack tuple
+(time, date)=timedate
 #read original csv into dataframe
-df=pd.read_csv(csv_name, header=None, names=ids, low_memory=False)
+numbers=[]
+for n in range(1,(len(ids)+1)):
+    numbers.append(n)
+print("Columns to be used: ", numbers)
+try:
+    df=pd.read_csv(csv_name, header=None, names=ids, usecols=numbers, low_memory=False)
+except:
+    print("An error occured reading the csv file.")
 #create new dataframe
 df_mod=pd.DataFrame()
 #cycle columns from data frame through repair function
@@ -82,5 +131,9 @@ sfreq = samplefrequency(frequency)
 
 #turn dataframe into a csv
 df_mod.to_csv(new_csv_name, index=False)
-wfdb.io.csv2mit(new_csv_name,fs=sfreq, units=units)
-
+#convert csv file to hea and dat
+print("Converting to MIT format")
+try:
+    wfdb.io.csv2mit(new_csv_name,fs=sfreq, units=units, samps_per_frame=1, base_time=time, base_date=date)
+except:
+    print("An error occurred when converting to MIT format")
